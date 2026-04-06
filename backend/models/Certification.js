d2b923cbdd4fe3a56239471bdcb910d22640f29c
pstrenth <prestonpstrenth@gmail.com>
2026-04-06 16:07:18 -0500

    Add Certification model with schema definition

---
+++ backend/models/Certification.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const certificationSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  issuer: { type: String, required: true },
+  date: { type: Date },
+  description: { type: String },
+  username: { type: String, required: true }
+});
+
+module.exports = mongoose.model("Certification", certificationSchema);
